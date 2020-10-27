ff6e2cb28016b08e886e4afcc407906e888ae952
Amlan <amlannandy5@gmail.com>
2020-10-27 13:04:48 +0530

    initialized middleware

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -2,7 +2,13 @@
 // @route         GET /api/v1/bootcamps
 // @access        Public
 exports.getBootcamps = (req, res, next) => {
-  res.status(200).json({ success: true, msg: 'Display all bootcamps' });
+  res
+    .status(200)
+    .json({
+      success: true,
+      msg: 'Display all bootcamps',
+      middleware: req.hello,
+    });
 };
 
 // @description   Get single bootcamps
